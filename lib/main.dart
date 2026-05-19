e7a91d0c831667d3287df77623de0abd5bebe1b0
Juan Cruz <tonocruz9000@gmail.com>
2026-05-18 21:32:41 -0600

    Setup the state managment with Riverpod and Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:stock_sense/core/theme/app_theme.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'core/theme/app_theme.dart';
+import 'core/navigation/root_shell.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Local cache
+  await Hive.initFlutter();
+  await Hive.openBox('userPreferences');
+
+  // Supabase — uses local instance; swap URL + key for production.
   await Supabase.initialize(
     url: 'http://127.0.0.1:54321',
     anonKey: 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH',
   );
 
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -20,13 +33,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Stock Sense',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme,
-
-      home: const Scaffold(
-        body: Center(
-          child: Text('Supabase Initialized Successfully!'),
-        ),
-      ),
+      home: const RootShell(),
     );
   }
 }
